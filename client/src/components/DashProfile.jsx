23725c84a9892427d78e29e2944c5e51604a5563
Rabin Kumar Baniya <rabinkrbaniya09@.com>
2024-02-03 14:54:30 +0545

    added a create a post page ui and its API route

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -3,10 +3,10 @@ import { useState } from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import { updateStart,updateSuccess,updateFailure,deleteUserStart,deleteUserSuccess,deleteUserFailure ,signoutSuccess} from '../redux/user/userSlice';
 import {HiOutlineExclamationCircle} from 'react-icons/hi';
-
+import {Link} from 'react-router-dom';
 
 export default function DashProfile() {
-  const {currentUser} = useSelector(state => state.user);
+  const {currentUser, loading} = useSelector(state => state.user);
   const [formData, setFormData] = useState({});
   const dispatch = useDispatch();
   const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
@@ -90,9 +90,18 @@ export default function DashProfile() {
         <TextInput type='text' id='username' placeholder='username' defaultValue={currentUser.username} onChange={handleChange} />
         <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email} onChange={handleChange}  />
         <TextInput type='password' id='password' placeholder='password' onChange={handleChange} />
-        <Button type='submit' gradientDuoTone='purpleToBlue' outline>
-          Update
+        <Button type='submit' gradientDuoTone='purpleToBlue' outline disabled={loading}>
+          {loading ? 'Loading...' : 'Update'}
         </Button>
+        {
+          currentUser.isAdmin && (
+            <Link to={'/create-post'}>
+              <Button type='button' gradientDuoTone='purpleToPink' className='w-full'>
+              create a post
+            </Button>
+            </Link>
+          )
+        }
       </form>
       <div className='text-red-500 flex justify-between mt-5'>
         <span onClick={()=>setShowModal(true)} className='cursor-pointer'>Delete Account</span>
