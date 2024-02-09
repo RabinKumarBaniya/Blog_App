67d3f23f4c83ed746ad0a9b4594c44b51f74808f
Rabin Kumar Baniya <rabinkrbaniya09@.com>
2024-02-09 21:29:32 +0545

    completed the home page

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -6,7 +6,7 @@ export default function CallToAction() {
   return (
     <div className='flex flex-col sm:flex-row p-3 border border-red-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center'>
     <div className='flex-1 justify-center flex flex-col sm:items-center sm:text-center'>
-        <h2 className='text-2xl'>Rabin Kumar Baniya</h2>
+        <h2 className='text-2xl text-black'>Rabin Kumar Baniya</h2>
         <p className='text-gray-500 my-2'> 
             Enthusiastic Designing and Developing Web Application
         </p>
